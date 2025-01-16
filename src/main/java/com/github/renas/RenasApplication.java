5f458650a3b4cf2d407a2aad47425c39826ba9ec
renasustek <renasustek@gmail.com>
2025-01-16 13:16:29 +0000

    First commit

---
+++ src/main/java/com/github/renas/RenasApplication.java
@@ -0,0 +1,13 @@
+package com.github.renas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenasApplication.class, args);
+	}
+
+}
